7fc2c86ee014285193f718ae980bab957ee6b400
natasha_s <natasl1012@gmail.com>
2023-07-05 23:26:54 +0300

    added dztask2

---
+++ dztask2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая на вход 
+// принимает два числа и выдаёт, какое число большее, а 
+// какое меньшее
+
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 == number2)
+{
+    Console.WriteLine("Ваши числа равны");
+}
+
+else if (number1 > number2)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+
+else
+{
+    Console.WriteLine("Второе число больше первого");
+}
